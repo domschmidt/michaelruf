bee666560f87aec3d5bb5cdcd166b0fd2356776b
Dominik Schmidt <dominik.mail@gmail.com>
2015-07-08 21:05:57 +0200

    HTML structure draft

---
+++ resources/js/controller/busroutes.js
@@ -0,0 +1,10 @@
+app.controller('BusRoutesCtl', ['$scope', function ($scope) {
+  $scope.routes = [
+    {'name': 'Nexus S',
+     'snippet': 'Fast just got faster with Nexus S.'},
+    {'name': 'Motorola XOOM™ with Wi-Fi',
+     'snippet': 'The Next, Next Generation tablet.'},
+    {'name': 'MOTOROLA XOOM™',
+     'snippet': 'The Next, Next Generation tablet.'}
+  ];
+}]);
